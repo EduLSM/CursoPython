00de623284a51b576e763ee101fa322a14b07a3b
Eduardo Skaf <skafm.eduardo@gmail.com>
2022-08-22 22:48:01 -0300

    correçao ex010

--- ex010.py
+++ ex010.py
@@ -2,7 +2,7 @@ cot = float(5.17)
 BRL = float(input('Quanto você tem na carteira hoje? '))
 USD = BRL/cot
 if BRL < 0:
-    print('Erro: reinicie e tente novamente com um valor Positivo')
+    print('Erro: reinicie e tente novamente com um valor aceitavel')
 
 else:
     print('A cotação do USD hoje 22.08.2022 é de R$: {}'.format(cot))
