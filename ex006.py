a8d344f2a3df317647894a3bfca5247f3ab96a09
Eduardo Skaf <skafm.eduardo@gmail.com>
2022-08-29 20:46:54 -0300

    ex 006

--- ex006.py
+++ ex006.py
@@ -1,6 +1,7 @@
+import math
 n = float(input('Digite um número: '))
 db = n * 2
 tr = n * 3
-rq = n ** (1 / 2)
+raiz = math.sqrt(n)
 print('Seu número escolhido foi {}\n o dobro é {}\n o triplo é {}'.format(n, db, tr))
-print(' e a raiz quadrada é {}'.format(rq))
+print(' e a raiz quadrada de {} é {}'.format(n, raiz))
